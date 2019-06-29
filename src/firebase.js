799fd84123479012b32f10589baa994efd235fb3
Marcelo Ratton <rattones@marceloratton.com>
2019-06-29 07:06:34 -0300

    connecting to firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+( () => {    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyDXkGZ2H9FEchBY9VOpM9IaVy91pciaJ1E",
+        authDomain: "tic-tac-toe-43443.firebaseapp.com",
+        databaseURL: "https://tic-tac-toe-43443.firebaseio.com",
+        projectId: "tic-tac-toe-43443",
+        storageBucket: "",
+        messagingSenderId: "305597501562",
+        appId: "1:305597501562:web:0f94259ab9fdd750"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+})()
